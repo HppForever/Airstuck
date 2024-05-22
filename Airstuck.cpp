0fb0ab1323c11ab8396ea3697a8eff68e548f297
Миша <125406303+HppForever@users.noreply.github.com>
2024-05-22 23:43:47 +0300

    Update Airstuck.cpp

--- Airstuck.cpp
+++ Airstuck.cpp
@@ -1,16 +1,16 @@
-class CAirStuck
+class AirStuck
 {
-private: void Set_MoveValue(float forwardmove, float sidemove)
-	{
-		usercmd_s* pCmd = cmd::get();
+void Set_MoveValue(float forwardmove, float sidemove)
+{
+	usercmd_s* pCmd = cmd::get();
 
-		pCmd->forwardmove = forwardmove;
-		pCmd->sidemove = sidemove;
-	}
+	pCmd->forwardmove = forwardmove;
+	pCmd->sidemove = sidemove;
+}
 
 public: void CL_CreateMove()
 	{
-		float AirStuck_POWER = 0;
+		float Power = NULL;
 
 		if (cvar.movement.airstuck_enabled) return;
 
@@ -19,24 +19,33 @@ public: void CL_CreateMove()
 
 		switch (cvar.movement.airstuck_power)
 		{
-			case 1: AirStuck_POWER = 0.955; break;
-			case 2: AirStuck_POWER = 0.975; break;
-			case 3: AirStuck_POWER = 0.985; break;
-			case 4: AirStuck_POWER = 0.999; break;
-			case 5: AirStuck_POWER = 0.9999; break;
+			case 1: Power = 0.955; break;
+			case 2: Power = 0.975; break;
+			case 3: Power = 0.985; break;
+			case 4: Power = 0.999; break;
+			case 5: Power = 0.9999; break;
 		}
 
 		if (cvar.movement.airstuck_enabled && cvar.movement.airstuck_key)
 		{
-			g_Utils.AdjustSpeed(1.0 - AirStuck_POWER);
+			g_Utils.AdjustSpeed(1.0 - Power);
 
 			if (cvar.movement.airstuck_autoreduce_speed)
 			{
-				if (g_Local.m_flVelocity > 200)       Set_MoveValue(-1000, -1000);
-				else if (g_Local.m_flVelocity <= 200) Set_MoveValue(-100, -100);
-				else if (g_Local.m_flVelocity <= 120) Set_MoveValue(-10, -10);
-				else if (g_Local.m_flVelocity <= 50)  Set_MoveValue(-1, -1);
-				else if (g_Local.m_flVelocity <= 0)   Set_MoveValue(0, 0);
+				if (g_Local.m_flVelocity > 200)        
+					Set_MoveValue(-1000, -1000);
+					
+				else if (g_Local.m_flVelocity <= 200)  
+					Set_MoveValue(-100, -100);
+					
+				else if (g_Local.m_flVelocity <= 120)  
+					Set_MoveValue(-10, -10);
+					
+				else if (g_Local.m_flVelocity <= 50)  
+					Set_MoveValue(-1, -1);
+					
+				else if (g_Local.m_flVelocity <= NULL)
+					Set_MoveValue(NULL, NULL);
 			}
 		}
 	}
